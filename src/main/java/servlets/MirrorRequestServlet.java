71d88a4802ff61fcb3b315e25777a1b842bf6cd5
swasta <link.bor@gmail.com>
2015-12-12 22:01:54 +0300

    create server

---
+++ src/main/java/servlets/MirrorRequestServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class MirrorRequestServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+        Map<String, Object> pageVariables = createPageVariablesMap(request);
+        String value = request.getParameter("key");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (value == null || value.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+        pageVariables.put("key", value == null ? "" : value);
+
+        response.getWriter().println(value);
+
+    }
+
+    private static Map<String, Object> createPageVariablesMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        return pageVariables;
+    }
+}

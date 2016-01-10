567a1c1b82d2a807b393b8a36cc2a614d9f2ed1f
swasta <link.bor@gmail.com>
2016-01-10 16:43:23 +0300

    Authorization add

---
+++ 2. Authorization/L2.1 Authorization/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by nikitaborodulin on 10/01/16.
+ */
+public class SignUpServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        //check if login is available
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile != null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().println("This login is already taken, please choose another");
+            return;
+        }
+
+        UserProfile newProfile = new UserProfile(login, pass, login);
+        accountService.addNewUser(newProfile);
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("Successful registration!");
+    }
+}
